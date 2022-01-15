0eaf395370d93db07a0ab39ac0a7b84413586614
Mar9740 <mar9740@rit.edu>
2022-01-15 17:04:54 -0500

    Added a zoo with a list of mammals. Basic inheritance

---
+++ DataStructures/Mammal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Mammal
+    {
+        protected int age;
+        protected bool isMale;
+        protected string name;
+
+        public Mammal()
+        {
+            age = 0;
+            isMale = false;
+            name = null;
+        }
+
+        public override string ToString()
+        {
+            return "This is just a blank mammal";
+        }
+
+    }
+
+    public class Tiger : Mammal
+    {
+        protected string type;
+
+        public Tiger(int age, bool male, string name)
+        {
+            this.age = age;
+            isMale = male;
+            this.name = name;
+            type = "Tiger";
+        }
+
+        public void changeName(string newName)
+        {
+            this.name = newName;
+        }
+
+        public void birthday() {
+            age++;
+        }
+
+        public override string ToString()
+        {
+            return this.name+" is a "+type+" and is "+age+" years old";
+        }
+    }
+}
---
+++ DataStructures/Zoo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Zoo
+    {
+
+        static void Main(string[] args)
+        {
+            Mammal[] zooAnimals = new Mammal[5];
+
+            zooAnimals[0] = new Tiger(11, true, "Tony");
+            zooAnimals[1] = new Mammal();
+            zooAnimals[2] = new Mammal();
+            zooAnimals[3] = new Mammal();
+            zooAnimals[4] = new Mammal();
+
+
+
+            Zoo.printZoo(zooAnimals);
+        }
+
+        public static void printZoo(Mammal[] animals)
+        {
+            foreach(Mammal m in animals)
+            {
+                Console.WriteLine(m.ToString());
+            }
+        }
+    }
+}
